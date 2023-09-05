fcb4ee97a95c4966ff7353d50f25becb181afbf3
Fanpeng Li <serforfp@gmail.com>
2023-09-05 17:00:26 +0100

    add frontend folder

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,25 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 //Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for handling CORS policy
+app.use(
+  cors({
+    origin: "http://localhost:5000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 app.get("/", (request, response) => {
   console.log(request);
-  return response.status(200).send("Welcome");
+  return response.status(200).send("Welcome to the bookstore project.");
 });
 
 app.use("/books", booksRoute);
